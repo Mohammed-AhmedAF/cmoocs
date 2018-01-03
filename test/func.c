95e1a2d93d07fa5d40d14a0028b6fd4bd9a8dfe5
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-04 00:18:52 +0200

    testing parent include

---
+++ test/func.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "func.h"
+
+void sayHello(int num) {
+	while(num > 0) {
+		printf("Hello\n");
+		num--;
+	}
+}
+
+
---
+++ test/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "func.h"
+	
+void main(void) {
+	int num;
+	printf("Enter a number:\n");
+	scanf("%d",&num);
+	sayHello(num);
+}
